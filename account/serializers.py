04a431960e969379fcb5268c61f3476d235d63ed
surajkarki66 <suraj.karki500@protonmail.com>
2023-05-14 06:58:17 +0545

    feature(account):finished login serializer

--- account/serializers.py
+++ account/serializers.py
@@ -35,3 +35,47 @@ class SignUpSerializer(CountryFieldMixin, serializers.ModelSerializer):
                 'password': ["The two password fields didn't match."]
             })
         return attrs
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    class Meta:
+        fields = ('email', 'password', 'is_verified')
+        read_only_fields = ('is_verified', )
+
+    def validate(self, attrs):
+        """
+        It checks if the user exists, if the user is active and verified, and if the password is correct
+        
+        :param attrs: The validated data from the serializer
+        :return: The user object is being returned.
+        """
+        authenticate_kwargs = {
+            'email': attrs.get('email'),
+            'password': attrs.get('password')
+        }
+        try:
+            authenticate_kwargs['request'] = self.context['request']
+        except KeyError:
+            pass 
+        
+        account = User.objects.filter(email=attrs.get('email'))
+        if not account.exists():
+            raise ValidationError(
+                {'message': 'No account found with this email.'
+            })
+        
+        # user = account.first()
+        # if not (user.is_active and user.is_verified):
+        #     raise ValidationError({
+        #         'is_verified': user.is_verified,
+        #         'message': 'Please verify your email before login.'
+        #     })
+        
+        user = authenticate(**authenticate_kwargs)
+        if user is None:
+            raise ValidationError({
+                'message': 'Incorrect email or password.'
+            })
+        attrs['user'] = user
+        return attrs
