90b4b52dc41bf494422453d1d769c4bff631dc92
surajkarki66 <suraj.karki500@protonmail.com>
2023-05-14 06:58:37 +0545

    feature(account):finished login api

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from .views import SignUpAPIView
+from .views import SignUpAPIView, LoginAPIView
 
 
 urlpatterns = [
     path('signup/', SignUpAPIView.as_view(), name='signup-api'),
+    path('login/', LoginAPIView.as_view(), name='login-api'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 import os
 
+from rest_framework.views import APIView
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from email.mime.image import MIMEImage
 from django.db import transaction
 from django.utils import timezone
@@ -14,11 +15,13 @@ from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.template.loader import get_template
 
-from .serializers import SignUpSerializer
+from .serializers import SignUpSerializer, LoginSerializer
 from MediLeaf_backend.email_thread import EmailThread
 
 
 User = get_user_model()
+
+
 class SignUpAPIView(generics.CreateAPIView):
     serializer_class = SignUpSerializer
     queryset = User.objects.all()
@@ -27,7 +30,7 @@ class SignUpAPIView(generics.CreateAPIView):
     def send_email(self, user):
         """
         It sends an email to the user with a link to verify their account
-        
+
         :param user: The user object that is being sent the email
         """
         mail_subject = 'Verify your MediLeaf Account'
@@ -45,7 +48,6 @@ class SignUpAPIView(generics.CreateAPIView):
 
         with open(img_path, 'rb') as image_file:
             image_data = image_file.read()
-
 
         template = get_template('account/signupVerification.html')
 
@@ -71,27 +73,52 @@ class SignUpAPIView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         """
         It creates a user and sends a verification email to the user
-        
+
         :param request: The request object
         :return: A response object is being returned.
         """
-        
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
         validated_data.pop('confirm_password')
         password = validated_data.pop('password')
-        
+
         user = User(**validated_data)
         user.set_password(password)
         user.verification_link_expiration = timezone.now() + timezone.timedelta(hours=24)
         user.save()
 
-        x,y = Group.objects.get_or_create(name = 'user')
+        x, y = Group.objects.get_or_create(name='user')
         user.groups.add(x)
         self.send_email(user,)
-        
+
         return Response({
             'message': "We have sent a verification link to your email address. Please verify your account."
-        },status=status.HTTP_201_CREATED)
+        }, status=status.HTTP_201_CREATED)
 
+
+class LoginAPIView(APIView):
+    serializer_class = LoginSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        """
+        The function takes in a request, validates the request, logs the user in, and returns a response
+
+        :param request: The request object
+        :return: The response is returning a message and the expiration date of the session.
+        """
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        expired_date = str(request.session.get_expiry_date())
+        return Response({
+            'message': 'Login Successful',
+            'expired_date': expired_date,
+        }, status=status.HTTP_200_OK)
