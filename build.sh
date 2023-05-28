7f908f13674b1dfdf4eaecef4af8d30ae9573a49
surajkarki66 <suraj.karki500@protonmail.com>
2023-05-28 19:43:09 +0545

    fix: update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.10 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.10 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
