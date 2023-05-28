d7855186f3eedd9d23514c4ed9329d6f4096e86b
surajkarki66 <suraj.karki500@protonmail.com>
2023-05-28 21:01:28 +0545

    fix: changes python runtime

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.11.3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.11.3 manage.py makemigrations --noinput
+python3.11.3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.11.3 manage.py collectstatic --noinput --clear
