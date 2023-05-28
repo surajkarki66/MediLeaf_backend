c25bee2f6a98a0db00c09d26cda479a9553173f4
surajkarki66 <suraj.karki500@protonmail.com>
2023-05-28 21:50:18 +0545

    fix: setup new static files dir

--- MediLeaf_backend/settings.py
+++ MediLeaf_backend/settings.py
@@ -140,11 +140,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-else:
-    STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static',)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
