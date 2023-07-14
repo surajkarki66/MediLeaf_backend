f3a481c956b684866b4230d3336f178e9852da0b
surajkarki66 <suraj.karki500@protonmail.com>
2023-07-14 09:24:26 +0545

    feat: finished feedback module

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,7 +1,14 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
+from django.template.defaultfilters import slugify
 from django_ckeditor_5.fields import CKEditor5Field
+from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
 from utilities.models import TimeStamp
+from utilities.validators import ImageValidator
 
 
 class ContactUs(TimeStamp):
@@ -21,3 +28,98 @@ class ContactUs(TimeStamp):
         return f'{self.subject}'
 
 
+User = get_user_model()
+
+def validate_array_length(value):
+    if len(value) > 5:
+        raise ValidationError("Array must contain  5 or less than 5 elements")
+
+def get_upload_to(instance, filename):
+    scientific_name = slugify(instance.get_scientific_name())
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    new_filename = f"{scientific_name}_{current_datetime}_"
+    image_path = f"feedback-plants/{scientific_name}/{new_filename}"
+
+    return image_path
+
+class Feedback(TimeStamp):
+    Annual = 'annual'
+    Biennial = 'biennial'
+    Perennial = 'perennial'
+    Ephemeral = 'ephemeral'
+    Deciduous = 'deciduous'
+    Evergreen = 'evergreen'
+
+    Duration = (
+        (Annual, 'Annual'),
+        (Biennial, 'Biennial'),
+        (Perennial, 'Perennial'),
+        (Ephemeral, 'Ephemeral'),
+        (Deciduous, 'Deciduous'),
+        (Evergreen, 'Evergreen')
+    )
+    Herb = 'herb'
+    Shrub = 'shrub'
+    Tree = 'tree'
+    Graminoid = 'graminoid'
+    Subshrub = 'subshrub'
+    Vine = 'vine'
+
+    Growth = (
+        (Herb, 'Herb'),
+        (Shrub, 'Shrub'),
+        (Tree, 'Tree'),
+        (Graminoid, 'Graminoid'),
+        (Subshrub, 'Subshrub'),
+        (Vine, 'Vine')
+    )
+
+    common_names = ArrayField(models.CharField(
+        max_length=255, unique=True), size=5, unique=True, validators=[validate_array_length])
+    description = CKEditor5Field()
+    medicinal_properties = CKEditor5Field()
+    duration = models.CharField(max_length=10, choices=Duration)
+    growth_habit = models.CharField(max_length=10, choices=Growth)
+
+    family = models.CharField('family', max_length=255)
+    genus = models.CharField('genus', max_length=255)
+    species = models.CharField(
+        'species', max_length=255, null=True, blank=True, default=None)
+    
+    image = models.ImageField(upload_to=get_upload_to, validators=[
+        ImageValidator(size=1000*1024)])
+    
+    is_verified = models.BooleanField(default=False, verbose_name='Verified')
+
+    user = models.ForeignKey(
+        User, related_name='feedback', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Feedback'
+        verbose_name_plural = 'Feedbacks'
+        ordering = ('-id',)
+
+    def get_scientific_name(self):
+        if self.species is not None:
+            return f'{self.genus} {self.species}'.strip()
+        else:
+            return f'{self.genus}'
+
+    def __str__(self):
+        if self.species is not None:
+            return f'{self.genus} {self.species}'.strip()
+        else:
+            return f'{self.genus}'
+
+    def image_tag(self):
+        image = self.image
+        if image:
+            return mark_safe(
+                '<a href="%s" target = "_blank"><img src ="%s" style="width: 50px; height:50px;"/></a>' %
+                ("https://res.cloudinary.com/deek0shwx/image/upload/v1/" + str(image),
+                 "https://res.cloudinary.com/deek0shwx/image/upload/v1/"+str(image))
+            )
+        else:
+            return 'No Image Found'
+
+    image.short_description = 'Image'
--- contact_us/urls.py
+++ contact_us/urls.py
@@ -6,6 +6,7 @@ from contact_us import views
 router = DefaultRouter()
 
 router.register(r'contact_us', views.ContactUsViewSet, 'contact_us')
+router.register(r'contact/feedback', views.FeedbackViewset, 'feedback')
 
 
 app = 'contact_us'
