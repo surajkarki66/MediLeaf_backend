6da0bfb40add4a466c8a93569dbad64f557dd70a
surajkarki <suraj.karki500@protonmail.com>
2024-01-09 15:32:18 +0100

    add whitenoise and configured the app for production

--- MediLeaf_backend/wsgi.py
+++ MediLeaf_backend/wsgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MediLeaf_backend.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
 app = application
